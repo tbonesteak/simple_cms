45fcdd2c9fe3462c329c65c5fb32ec6771a3fac9
Jon Lue <tbonesteak@gmail.com>
2012-11-10 19:28:19 -0800

    first commit. There is a demo controller and view called index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'cfaf9daefc02070143959c4afc83da1c15047e927da1d644d9b215ec74d2023eecfca2afb9eaae00a01ba142ce06a80b3fb5f95fe1595189e2abb3601973506a'
